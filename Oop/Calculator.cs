99edbea079e0e84efc72c9021d448100ce159bac
JonjoGanaden <jonjoganaden@hotmail.com>
2022-04-01 15:29:35 +0100

    Initial commit

---
+++ Oop/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oop
+{
+    internal class Calculator
+    {
+        public int Multiply(int a, int b)
+        { 
+            return a * b;
+        }
+
+    }
+}
---
+++ Oop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oop
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            int x = 3;
+            int y = 5;
+            int sum = 0;
+
+            sum = calc.Multiply(x, y);
+            Console.WriteLine("sum of {0} * {1} * {2}", x, y, sum);
+            Console.ReadLine();
+        }
+    }
+}
